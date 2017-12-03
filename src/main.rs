b41ec4aface957d349839de7b263aa521ad8dbd1
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-04 08:12:54 +0900

    testing hello_world

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,33 @@
 
 extern crate rocket;
 
+use rocket::Rocket;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world"
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+fn rocket() -> Rocket {
+    rocket::ignite().mount("/", routes![hello])
 }
 
 fn main() {
-   rocket::ignite().mount("/", routes![index]).launch();
+   rocket().launch();
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("Hello, world!".into()));
+    }
 }
